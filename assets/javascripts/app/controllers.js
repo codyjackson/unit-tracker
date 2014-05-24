7c931511dcfdaa52d79c6f31b269649175ca1c0f
Cody Jackson <sxenog@gmail.com>
2014-05-23 23:39:07 -0700

    Put together an extract units function.

--- assets/javascripts/app/controllers.js
+++ assets/javascripts/app/controllers.js
@@ -1,4 +1,4 @@
-define(['angular', 'services'], function(angular){
+define(['angular', 'services', 'leaflet'], function(angular){
     angular.module('controllers', ['services']).
     controller('MainController', ['$scope', '$http', '$xmlParser', function($scope, $http, $xmlParser){
         $scope.units = [
@@ -10,9 +10,52 @@ define(['angular', 'services'], function(angular){
             }
         ];
 
+        function Unit(id, firstLocation) {
+            this.id = id;
+            this.locations = [firstLocation];
+        }
+
+        Unit.prototype.addLocation = function (location) {
+            this.locations.push(location);
+        };
+
+        function extractUnits(jsonRoot){
+            var units = {};
+            var timeFrames = jsonRoot.GMIMessage.Predictions.LG_LTPByFrame.COASet.TimeFrame;
+
+            //Ignore empty time frames
+            timeFrames = timeFrames.filter(angular.isObject);
+
+            function upsertUnit(rawUnit){
+                if(!(rawUnit.UnitID && rawUnit.Location))
+                    return;
+                var id = rawUnit.UnitID;
+                var lat = parseFloat(rawUnit.Location.Lat.__text);
+                var lng = parseFloat(rawUnit.Location.Lon.__text);
+                var location = L.latLng(lat, lng);
+
+                if(!units[id]) {
+                    units[id] = new Unit(id, location);
+                    return;
+                }
+
+                units[id].addLocation(location);
+            }
+
+            timeFrames.forEach(function(timeFrame){
+                if(!angular.isArray(timeFrame.Unit))
+                    return;
+
+                timeFrame.Unit.forEach(upsertUnit);
+            });
+
+            return units;
+        }
+
         $http.get('GMIMessageEx2.xml').then(function(response){
             var json = $xmlParser.parseXmlToJson(response.data);
             console.log(json);
+            console.log(extractUnits(json));
         });
     }]);
 });
