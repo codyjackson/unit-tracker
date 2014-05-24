2d2682706a3f140bc560d18022fb136f963bec93
Cody Jackson <sxenog@gmail.com>
2014-05-24 09:42:52 -0700

    -Added the ability to toggle paths on and off. -Calculate and display the distance traveled by each unit. -Tried adding some filtering but that's not working quite yet.

--- assets/javascripts/app/controllers.js
+++ assets/javascripts/app/controllers.js
@@ -5,10 +5,27 @@ define(['angular', 'services', 'leaflet'], function(angular){
         function Unit(id, firstLocation) {
             this.id = id;
             this.path = L.polyline([firstLocation]);
+            this.bounds = null;
         }
 
-        Unit.prototype.addLocation = function (location) {
-            this.path.addLatLng(location);
+        Unit.prototype.addLocation = function (latlng) {
+            this.path.addLatLng(latlng);
+            if(this.path._latlngs.length == 2)
+                this.bounds = L.latLngBounds(this.path._latlngs[0], this.path._latlngs[1]);
+            else
+                this.bounds.extend(latlng);
+        };
+
+        Unit.prototype.getDistance = function() {
+            var latLngs = this.path._latlngs;
+            if(latLngs.length < 2)
+                return 0;
+
+            var distance = 0;
+            for(var i = 0; i < latLngs.length - 1; ++i) {
+                distance += latLngs[i].distanceTo(latLngs[i + 1]);
+            }
+            return distance / 1000;
         };
 
         function extractUnits(jsonRoot){
@@ -48,5 +65,13 @@ define(['angular', 'services', 'leaflet'], function(angular){
             var json = $xmlParser.parseXmlToJson(response.data);
             $scope.units = extractUnits(json);
         });
+    }]).
+    controller('UnitLegendController', ['$scope', function($scope){
+        $scope.togglePath = function(unit) {
+            if(unit.showPath)
+                $scope.addToMap(unit.path);
+            else
+                $scope.removeFromMap(unit.path);
+        };
     }]);
 });
